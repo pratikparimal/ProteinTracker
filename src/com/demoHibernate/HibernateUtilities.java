f9688b0cef72cbb3f356e67e4d457461f1266a28
Pratik Parimal <Pratik Parimal@Pratik_Parimal>
2018-08-22 12:34:40 +0530

    updated the first commit

--- src/com/demoHibernate/HibernateUtilities.java
+++ src/com/demoHibernate/HibernateUtilities.java
@@ -5,12 +5,10 @@ import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtilities {
 	
 	public static SessionFactory sessionFactory;
-	public static ServiceRegistry serviceRegistry;
 	
 	static {
 		   try {
@@ -20,7 +18,7 @@ public class HibernateUtilities {
 		        new MetadataSources(standardRegistry).getMetadataBuilder().build();
 			sessionFactory = metaData.getSessionFactoryBuilder().build();
 		   } catch (Throwable th) {
-			System.err.println("Enitial SessionFactory creation failed" + th);
+			System.err.println("Initial SessionFactory creation failed" + th);
 			throw new ExceptionInInitializerError(th);
 		  }
 		}
