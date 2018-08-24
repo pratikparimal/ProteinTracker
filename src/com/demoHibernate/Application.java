81ca218925f866ff6715cc7910813321e98b1d49
Pratik Parimal <Pratik Parimal@Pratik_Parimal>
2018-08-24 11:11:37 +0530

    updated till 29

--- src/com/demoHibernate/Application.java
+++ src/com/demoHibernate/Application.java
@@ -8,14 +8,21 @@ public class Application {
 
 		Session session = HibernateUtilities.getSessionFactory().openSession();
 		session.beginTransaction();
-		
 		User user = new User();
 		user.setName("Pratik");
-		user.setTotal(500);
 		user.setGoal(50);
 		session.save(user);
+		session.getTransaction().commit();
+		
+		session.beginTransaction();
+		User loadUser = (User) session.get(User.class, 1);
+		System.out.println(loadUser.getName());
+		System.out.println(loadUser.getGoal());
+		
+		loadUser.setGoal(loadUser.getGoal()+100);
 		
 		session.getTransaction().commit();
+		
 		session.close();
 		HibernateUtilities.getSessionFactory().close();
 	}
